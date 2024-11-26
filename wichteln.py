f647667eb747fc4894f746c7c0453a03c4610acc
kamranvat <kamran.vat@gmail.com>
2024-11-26 13:09:10 +0100

    feat: Add participants.csv file

--- wichteln.py
+++ wichteln.py
@@ -1,8 +1,15 @@
 """random secret santa assignment script"""
+
+import csv
 from random import shuffle
 
-# Get all participants
-participants = ["Bob", "Ben", "Der Ganjamann", "Harro", "Jesus"]
+# Read participants from a .csv file
+participants = []
+with open("participants.csv", newline="") as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        participants.append(row[0])
+
 participants_total = len(participants)
 
 # Shuffle participants. Each participant gives a gift to the next one in the resulting list
@@ -10,7 +17,7 @@ shuffle(participants)
 
 # Print assigned santas
 for i in range(participants_total):
-	if i+1 < participants_total:
-		print(participants[i] + " -> " + participants[i+1])
-	else:
-		print(participants[i] + " -> " + participants[0])
+    if i + 1 < participants_total:
+        print(participants[i] + " -> " + participants[i + 1])
+    else:
+        print(participants[i] + " -> " + participants[0])
